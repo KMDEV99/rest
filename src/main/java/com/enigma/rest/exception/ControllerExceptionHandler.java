00b94608b0733997004ca77404e92b778571423a
KMDEV99 <matkonrad99@gmail.com>
2022-11-22 16:35:23 +0100

    Moved ControllerExceptionHandler

--- src/main/java/com/enigma/rest/exception/ControllerExceptionHandler.java
+++ src/main/java/com/enigma/rest/exception/ControllerExceptionHandler.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 import java.util.Date;
 
 @RestControllerAdvice
@@ -14,6 +16,18 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(WrongDueDateException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorMessage handleWrongDateDueException(WrongDueDateException ex, WebRequest request) {
+
+        return new ErrorMessage(
+                HttpStatus.BAD_REQUEST.value(),
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false)
+        );
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorMessage handleConstraintViolationError(ConstraintViolation ex, WebRequest request) {
 
         return new ErrorMessage(
                 HttpStatus.BAD_REQUEST.value(),
